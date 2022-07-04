c9b3e074411210ec90266adb7a5225394c9c933a
MoochontheMic <lachlanharding2000@gmail.com>
2022-07-04 13:32:59 +0100

    Frame of API set up

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -18,6 +18,13 @@ class Item(Resource):
     """
     Class to hold the get and post command from individual items
     """
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type = float,
+        required = True,
+        help = 'This field cannot be left blank'
+    )
+
     @jwt_required()
     def get(self, name:str):
         item = next(filter(lambda x:(x['name'] == name), items), None)
@@ -27,8 +34,8 @@ class Item(Resource):
 
         if next(filter(lambda x:(x['name'] == name), items), None):
             return {'message': "An item with name '{}' already exists".format(name)}, 400
-
-        data = request.get_json(force=True)
+        
+        data = self.parser.parse_args()
         item  = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -36,6 +43,20 @@ class Item(Resource):
     def delete(self, name):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'item deleted'}
+
+    def put(self, name):
+        
+        data = self.parser.parse_args()
+
+        item = next(filter(lambda x: x['name']== name ,items), None)
+        if not item:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+
+        return item
 
 class ItemList(Resource):
     def get(self):
