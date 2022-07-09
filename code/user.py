9975af4fe0fd29b107719db999d22e491cd75cf0
MoochontheMic <lachlanharding2000@gmail.com>
2022-07-09 12:16:15 +0100

    sql database created by python and can be added to

--- code/user.py
+++ code/user.py
@@ -1,3 +1,9 @@
+import sqlite3
+
+from colorama import Cursor
+
+
+
 class User:
     """
     Creates/initiaises a user for the system
@@ -6,3 +12,39 @@ class User:
         self.id = iD
         self.username = username
         self.password = password
+
+    @classmethod
+    def findByUsername(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'SELECT * FROM users WHERE username=?'
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+
+        else:
+            user = None
+        
+        connection.close()
+
+        return user
+
+    @classmethod
+    def findById(cls, iD):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'SELECT * FROM users WHERE id=?'
+        result = cursor.execute(query, (iD,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+            
+        else:
+            user = None
+        
+        connection.close()
+
+        return user
